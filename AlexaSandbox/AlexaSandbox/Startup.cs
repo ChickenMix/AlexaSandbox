df1eb92c92f017d61bf667a2f1b073874010c0c7
Matt <j.matt@hotmail.co.uk>
2021-05-03 19:35:01 +0100

    Fix

--- AlexaSandbox/AlexaSandbox/Startup.cs
+++ AlexaSandbox/AlexaSandbox/Startup.cs
@@ -26,8 +26,7 @@ namespace AlexaSandbox
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AlexaSandbox", Version = "v1" });
@@ -41,6 +40,12 @@ namespace AlexaSandbox
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AlexaSandbox v1"));
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AlexaSandbox v1"));
             }
